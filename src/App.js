eb757cde91811c898b70cf3be7d7aff1cf6b1f80
lebLuiz <leb.e.b@hotmail.com>
2023-07-04 23:21:33 -0300

    Utilizando useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import { ThemeProvider } from 'styled-components';
 
 import GlobalStyle from './styles/global';
@@ -7,7 +7,7 @@ import Layout from './components/Layout';
 import themes from './styles/themes';
 
 function App() {
-  const [theme, setTheme] = useState('dark');
+  const [theme, setTheme] = useState(JSON.parse(localStorage.getItem('theme') || 'dark'));
   const currentTheme = useMemo(() =>
     themes[theme] || themes.dark,
   [theme]);
@@ -19,13 +19,36 @@ function App() {
     );
   }
 
+  // 1º parametro: Função de efeito
+  /*
+  * Entendesse o useEffect como se fosse uma função chamada a cada vez que o componente/pagina
+  * é atualizado/renderizado.
+  * "Mas eu só quero que seja chamado na primeira vez q é montado o componente/pagina":
+  * Simples, passe um array vazio.
+  * "Mas se eu quero alterar o valor de um estado quando renderizar/autalizar um componente/pagina?"
+  * Passe a definição do estado dentro do array, isso signfica que o useEffect será chamado quando aquela propriedade de estado ser alterada.
+  */
+
+  useEffect(() => {
+    console.debug('UseEffect executou ao alterar `theme`!');
+    localStorage.setItem('theme', JSON.stringify(theme));
+  }, [theme]);
+
   return (
     <ThemeProvider theme={currentTheme}>
       <GlobalStyle />
-      <Layout
-        selectedTheme={theme}
-        onToggleTheme={handleToggleTheme}
-      />
+      
+      <button onClick={handleToggleTheme}>Toggle</button>
+      {theme === 'dark' && (
+        /* 1º Aqui é um caso para mostrar o 'unmounted' do lifecycle do React.
+        * Com essa condição, caso seja false, o React de fato não renderiza no DOM,
+        * ou seja, há uma "desmontagem" do componente Layout (vá até o componente Layout)
+        */
+        <Layout
+          selectedTheme={theme}
+          onToggleTheme={handleToggleTheme}
+        />
+      )}
     </ThemeProvider>
   );
 };
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import Header from '../Header';
 import PostsList from '../PostsList';
@@ -7,6 +7,16 @@ import Footer from '../Footer';
 export default function Layout({
   onToggleTheme, selectedTheme,
 }) {
+  /* 2º Nesse useEffect é onde posso identificar o 'unmounted' q é feito lá no componente App.
+  * Segue o conceito normal do useEffect, porém, onde identifico a "desmontagem" de fato, é
+  * na função passada no `return`.
+  */
+  useEffect(() => {
+    console.debug({ selectedTheme });
+    return () => {
+      console.debug('<Layout/> saiu da tela.');
+    }
+  }, [selectedTheme]);
 
   return (
     <>
