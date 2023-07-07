4670a62d07f25576cfc9c1cb18d46815879b0adc
lebLuiz <leb.e.b@hotmail.com>
2023-07-06 23:02:32 -0300

    Utilizando ClassComponent e uso do Binding em metodos

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,22 +2,54 @@ import React from 'react';
 
 import { Container } from './styles';
 
-export default function Header({
-  onToggleTheme, selectedTheme,
-}) {
-  return (
-    <Container>
-      <h1>JStack's Blog</h1>
-      <button
-        type="button"
-        onClick={onToggleTheme}
-      >
-        {
-          selectedTheme === 'dark'
-            ? '🌞'
-            : '🌚'
-        }
-      </button>
-    </Container>
-  );
+/* NOTE: Utilizando com Class Component.
+* Todo componente de ClassComponente PRECISA extender React.Componente
+*/
+export default class Header extends React.Component {
+
+  render() {
+    const {
+      onToggleTheme,
+      selectedTheme,
+    } = this.props;
+
+    return (
+      <Container>
+        <h1>JStack`s Blog</h1>
+        <button
+          type="button"
+          onClick={onToggleTheme}
+        >
+          {
+            selectedTheme === 'dark'
+              ? '🌞'
+              : '🌚'
+          }
+        </button>
+      </Container>
+    );
+  }
+
 }
+
+
+// NOTE: Utilizando com Hook(Functional Component)
+// export default function Header({
+//   onToggleTheme, selectedTheme,
+// }) {
+//   return (
+//     <Container>
+//       <h1>JStack's Blog</h1>
+//       <button
+//         type="button"
+//         onClick={onToggleTheme}
+//       >
+//         {
+//           selectedTheme === 'dark'
+//             ? '🌞'
+//             : '🌚'
+//         }
+//       </button>
+//     </Container>
+//   );
+// }
--- src/components/PostsList/index.js
+++ src/components/PostsList/index.js
@@ -5,16 +5,40 @@ import Post from './Post';
 
 import posts from './posts';
 
-export default function PostsList() {
-  return (
-    <Container>
-      {posts.map((post) => (
-        <Post
-          key={post.id}
-          title={post.title}
-          description={post.description}
-        />
-      ))}
-    </Container>
-  );
+/* NOTE: Utilizando com Class Component.
+* Todo componente de ClassComponente PRECISA extender React.Componente
+*/
+export default class PostsList extends React.Component {
+
+  // PRECISA definir o render!
+  render() {
+    return (
+      <Container>
+        {posts.map((post) => (
+          <Post
+            key={post.id}
+            title={post.title}
+            description={post.description}
+          />
+        ))}
+      </Container>
+    )
+  }
+
 }
+
+
+// NOTE: Utilizando com Hook(Functional Component)
+// export default function PostsList() {
+//   return (
+//     <Container>
+//       {posts.map((post) => (
+//         <Post
+//           key={post.id}
+//           title={post.title}
+//           description={post.description}
+//         />
+//       ))}
+//     </Container>
+//   );
+// }
