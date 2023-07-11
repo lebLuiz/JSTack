c27ddbdaec58cb3b1dd65486dd6228a9a7097df5
lebLuiz <leb.e.b@hotmail.com>
2023-07-10 23:48:38 -0300

    BONUS - HOC para monitorar alterações na Context API [ClassComponent with FunctionComponent]

--- styled-theming-base/src/components/Header/index.js
+++ styled-theming-base/src/components/Header/index.js
@@ -5,34 +5,50 @@ import { ThemeContext } from '../../contexts/ThemeContext';
 
 import { Container } from './styles';
 
+function HOC(ComponentHeader) {
+  return class Component extends React.Component {
+    render(){
+      return (
+        <ThemeContext.Consumer>
+          {(value) => (
+            <ComponentHeader {...value} />
+          )}
+        </ThemeContext.Consumer>
+      );
+    }
+  }
+}
+
 /* NOTE: Utilizando com Class Component.
 * Todo componente de ClassComponente PRECISA extender React.Componente
 */
-export default class Header extends React.Component {
+class Header extends React.Component {
+  componentDidUpdate(prevProps, prevState) {
+    if (this.props.theme !== prevProps.theme)
+      console.log('Tema mudou');
+  }
 
   render() {
     return (
-      <ThemeContext.Consumer>
-        {({ theme: selectedTheme, handleToggleTheme }) => (
-          <Container>
-            <h1>JStack`s Blog</h1>
-            <button
-              type="button"
-              onClick={handleToggleTheme}
-            >
-              {
-                selectedTheme === 'dark'
-                  ? '🌞'
-                  : '🌚'
-              }
-            </button>
-          </Container>
-        )}
-      </ThemeContext.Consumer>
+      <Container>
+        <h1>JStack`s Blog</h1>
+        <button
+          type="button"
+          onClick={this.props.handleToggleTheme}
+        >
+          {
+            this.props.theme === 'dark'
+              ? '🌞'
+              : '🌚'
+          }
+        </button>
+      </Container>
     );
   }
 
 }
+
+export default HOC(Header);
 
 // CASO 2 - NOTE: Posso utilizar `propTypes` assim também.
 // Header.propTypes = {
