f642c16b48bde38bdd0c2c915a5891412acec5a7
lebLuiz <leb.e.b@hotmail.com>
2023-07-18 23:25:43 -0300

    Iniciando criação de components em mycontacts-app

---
+++ mycontacts-app/fe/src/components/ContactsList/index.js
@@ -0,0 +1,12 @@
+import { Container, Header } from './styles';
+
+export default function ContactsList() {
+  return (
+    <Container>
+      <Header>
+        <strong>3 Contatos</strong>
+        <a href="/">Novo Contato</a>
+      </Header>
+    </Container>
+  );
+}
--- mycontacts-app/fe/src/index.js
+++ mycontacts-app/fe/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/App';
+
+window.process = {};
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
